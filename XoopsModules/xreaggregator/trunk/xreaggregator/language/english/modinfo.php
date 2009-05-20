ca64cb30027e4d49b4a0e51e066bed6730d4888c
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-05-20 02:18:59 +0000

    X-Reaggregator 1.13 (Release)

---
+++ XoopsModules/xreaggregator/trunk/xreaggregator/language/english/modinfo.php
@@ -0,0 +1,29 @@
+<?php
+// $Id: modinfo.php,v 2.09 2008/06/10 15:04:00 wishcraft Exp $
+// Module Info
+
+// The name of this module
+define("_XA_REAGGREGATOR_NAME","X-Aggregator");
+
+// A brief description of this module
+define("_XA_REAGGREGATOR_DESC","Displayes RSS/XML Newsfeed from other sites Re-aggregates them");
+
+// Names of blocks for this module (Not all module has blocks)
+define("_XA_REAGGREGATOR_BNAME","Re-aggregator");
+
+// Names of admin menu items
+define("_XA_REAGGREGATOR_ADMENU1","List Re-aggregation");
+define("_XA_REAGGREGATOR_ADMENU2","RSS Mashables");
+
+define("_XA_RSS_UTF8", "RSS UTF Code");
+define("_XA_RSS_UTF8_DESC", "Set your RSS UTF Code here");
+define("_XA_RSS_CACHETIME", "RSS Cache time");
+define("_XA_RSS_CACHETIME_DESC", "Set the number of minutes for your RSS cache");
+define("_XA_SNOOP_QUANTITY", "RSS Snoop Quantity");
+define("_XA_SNOOP_QUANTITY_DESC", "Set the number items in an overall RSS Snoop");
+define("_XA_HTACCESS", ".htaccess SEO");
+define("_XA_HTACCESS_DESC", "Set this if you have installed the htaccess SEO in your XOOPS Root Path");
+define("_XA_CACHEME", "RSS Cache");
+define("_XA_CACHEME_DESC", "Set this if you want the RSS Re-aggregation cached.");
+
+?>
