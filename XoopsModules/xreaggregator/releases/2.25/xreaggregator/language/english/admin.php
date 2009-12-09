05c36810e811585c9da949d00be4981b5f3d7227
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-12-09 04:34:56 +0000

    Modules Final Releases - Compounds 2.00, Lawsuit 1.12, Multisite 1.30, Spiders 1.10, VidShop 1.60, VJ Mixes 1.20, Wurlf 1.26, xpNews 1.15, xReaggregator 2.25

---
+++ XoopsModules/xreaggregator/releases/2.25/xreaggregator/language/english/admin.php
@@ -0,0 +1,39 @@
+<?php
+// $Id: admin.php,v 2.09 2008/06/10 15:04:00 wishcraft Exp $
+//%%%%%%        Admin Module Name  xreaggregators         %%%%%
+define('_AM_DBUPDATED','Database Updated Successfully!');
+define('_AM_XREAGGREGATORS','Re-aggregation Configuration');
+define('_AM_HLMAIN','Re-aggregation Main');
+define('_AM_SITENAME','Site Name');
+define('_AM_URL','URL');
+define('_AM_ORDER', 'Order');
+define('_AM_ENCODING', 'RSS Encoding');
+define('_AM_CACHETIME', 'Cache Time');
+define('_AM_MAINSETT', 'Main Page Settings');
+define('_AM_BLOCKSETT', 'Block Settings');
+define('_AM_DISPLAY', 'Display in main page');
+define('_AM_DISPIMG', 'Display image');
+define('_AM_DISPFULL', 'Display in full view');
+define('_AM_DISPMAX', 'Max items to display');
+define('_AM_ASBLOCK', 'Display in block');
+define('_AM_ADDHEADL','Add xreaggregator');
+define('_AM_URLEDFXML','URL of RDF/RSS file');
+define('_AM_EDITHEADL','Edit xreaggregator');
+define('_AM_WANTDEL','Are you sure you want to delete aggregation for %s?');
+define('_AM_INVALIDID', 'Invalid ID');
+define('_AM_OBJECTNG', 'Object does not exist');
+define('_AM_FAILUPDATE', 'Failed saving data to database for aggregation %s');
+define('_AM_FAILDELETE', 'Failed deleting data from database for aggregation %s');
+define('_AM_DOMAINS', 'Domains Displayed In');
+define('_AM_MASHNAME', 'Mashable\'s Link Name');
+define('_AM_MASHGROUP', 'Mashable\'s Grouped Feeds');
+define('_AM_ADDMASHABLE', 'Add New RSS Mashable Scoop');
+define('_AM_RANDOM', 'Random Scoop');
+define('_AM_XREAGGREGATORS_MASHABLES', 'RSS Mashable\'s');
+define('_AM_CATEGORY', 'Category');
+define('_AM_CATEGORYNAME', 'Category Name');
+define('_AM_ADDCATEGORY', 'Add a Category');
+define('_AM_EDITCATEGORY', 'Edit Category');
+define('_AM_XREAGGREGATORS_CATEGORIES', 'Available Category');
+define('_AM_KEYWORDS', 'Filter keywords<br/>Seperate with a comma');
+?>
